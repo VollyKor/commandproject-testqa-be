cef4502693ed1d846504034de16a8275eaa6c812
Warness <65484013+Warness@users.noreply.github.com>
2021-04-04 22:52:23 +0300

    fix few problems

--- dist/bin/server.js
+++ dist/bin/server.js
@@ -5,8 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = __importDefault(require("../index"));
 const db_1 = __importDefault(require("../db/db"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+// import dotenv from 'dotenv'
+// dotenv.config()
 const PORT = process.env.PORT || 3010;
 // Connect to Database
 // ======================================
