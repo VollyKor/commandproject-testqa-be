a8eb70e5c20b268a399a25289dd9f916e5a5f5f4
Warness <65484013+Warness@users.noreply.github.com>
2021-04-04 22:31:56 +0300

    try to fix port

--- src/bin/server.ts
+++ src/bin/server.ts
@@ -1,7 +1,7 @@
 import app from '../index'
 import db from '../db/db'
-import dotenv from 'dotenv'
-dotenv.config()
+// import dotenv from 'dotenv'
+// dotenv.config()
 
 const PORT = process.env.PORT || 3010
 
