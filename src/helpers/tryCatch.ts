c602e02ad757d8b0e6f1fcb4f6580b4ac02ad048
Warness <volydemar2032@gmail.com>
2021-04-11 20:03:06 +0300

    1

--- src/helpers/tryCatch.ts
+++ src/helpers/tryCatch.ts
@@ -1,8 +1,19 @@
-import { RequestHandler } from 'express';
+// import { RequestHandler } from 'express';
+// import { ControllerFunction } from '../types/interfaces';
 import { ControllerFunction } from '../types/interfaces';
+
+// export default (cb: ControllerFunction): any =>
+//   ((req, res, next) => {
+//     cb(req, res, next).catch((err: Error) => next(err));
+//   }) as RequestHandler;
+
+import { Request, Response, NextFunction } from 'express';
 // import { ControllerFunction } from '../typescript-helpers/types';
 
-export default (cb: ControllerFunction): any =>
-  ((req, res, next) => {
-    return cb(req, res, next).catch((err: Error) => next(err));
-  }) as RequestHandler;
+export default (cb: ControllerFunction) => (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  return cb(req, res, next).catch((err: any) => next(err));
+};
