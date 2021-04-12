d6b290550aaf06951a9c774aac799c96ac7145b1
Warness <volydemar2032@gmail.com>
2021-04-12 12:33:11 +0300

    fix srver port

--- src/bin/server.ts
+++ src/bin/server.ts
@@ -1,17 +1,17 @@
-import app from '../index'
-import db from '../db/db'
-// import dotenv from 'dotenv'
-// dotenv.config()
+import app from '../index';
+import db from '../db/db';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const PORT = process.env.PORT || 3010
+const PORT = process.env.PORT || 3010;
 
 // Connect to Database
 // ======================================
 db.then(() => {
-    app.listen(PORT, () => { 
-        console.log(`Server running. Use our API on port: ${PORT}`)
-    })
-}).catch(e  => {
-    console.log(`Server not running. Error message: ${e.message}`)
-    process.exit(1)
-})
+  app.listen(PORT, () => {
+    console.log(`Server running. Use our API on port: ${PORT}`);
+  });
+}).catch(e => {
+  console.log(`Server not running. Error message: ${e.message}`);
+  process.exit(1);
+});
