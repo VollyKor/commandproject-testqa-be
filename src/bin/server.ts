2126a3c1d55b016cb701103e86d2ab7193356e03
Vitaliy Zvieriev <zverew91@gmail.com>
2021-04-14 15:41:01 +0300

    Add path /user/current

--- src/bin/server.ts
+++ src/bin/server.ts
@@ -1,15 +1,8 @@
 import app from '../index';
 import db from '../db/db';
-<<<<<<< HEAD
 
 import dotenv from 'dotenv';
 dotenv.config();
-=======
-import dotenv from 'dotenv';
-dotenv.config();
-
-const PORT = process.env.PORT || 3010;
->>>>>>> master
 
 const PORT = process.env.PORT || 3010;
 
